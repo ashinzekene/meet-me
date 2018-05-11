9b724b620d4bef5cdde4c99d2911c9ffebe5b5fc
Ashinzekene <ashinzekene@gmail.com>
2018-05-11 04:36:47 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgMaterialModule } from "./ng-material/ng-material.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
